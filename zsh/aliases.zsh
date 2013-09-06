9cf606e757bb3d4b23d62bb0c8e2e0a1d0b2e059
Philip Roberts <latentflip@gmail.com>
2013-09-06 15:19:15 +0100

    Fix zsh for rvm newest version

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,7 @@
 alias reload!='. ~/.zshrc'
 
+alias clr='clear'
+
 alias e='vim'
 alias le='vim -c "normal '\''0"'
 alias lep='vim -c "normal ,lep"'
@@ -10,3 +12,6 @@ alias tunnel='ssh -R 56985:localhost:5000 latentflip@latentflip.com'
 alias fack='find . -name '
 
 alias git='hub'
+alias unused='nocorrect unused'
+
+alias sj='noglob sj'
